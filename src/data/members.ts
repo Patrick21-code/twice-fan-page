8a5d720c560fab777f8760e660c9e8f6e9c65c5d
patpat <tomaspatrickdanielle@gmail.com>
2026-06-07 20:47:02 +0800

    feat: members data

---
+++ src/data/members.ts
@@ -0,0 +1,11 @@
+export const members = [
+    { name: "Nayeon", role: "Lead Vocalist", image: "/images/members/nayeon.jpg"},
+    { name: "Jeongyeon", role: "Lead Vocalist", image: "/images/members/jeongyeon.jpg" },
+    { name: "Momo", role: "Main Dancer", image: "/images/members/momo.jpg" },
+    { name: "Sana", role: "Vocalist", image: "/images/members/sana.jpg" },
+    { name: "Jihyo", role: "Leader", image: "/images/members/jihyo.jpg" },
+    { name: "Mina", role: "Main Dancer", image: "/images/members/mina.jpg" },
+    { name: "Dahyun", role: "Rapper", image: "/images/members/dahyun.jpg" },
+    { name: "Chaeyoung", role: "Rapper", image: "/images/members/chaeyoung.jpg" },
+    { name: "Tzuyu", role: "Visual", image: "/images/members/tzuyu.jpg" }
+]
